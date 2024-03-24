4b5f50a680bbe17cc90b06bb1b3c379f635dc13f
csigezoltan <info@csigezoltan.hu>
2024-03-24 16:02:35 +0100

    Api example

---
+++ app/lib/api/apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './HealthApi';
+export * from './LetterApi';
+export * from './PageApi';
---
+++ app/lib/api/models/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ExampleDTO';
+export * from './GrammaticalExpressionDTO';
+export * from './HealthCheckDTO';
+export * from './LetterDTO';
+export * from './ModelGrammaticalExpressionType';
+export * from './PageDTO';
+export * from './TranslationDTO';
+export * from './WordDTO';
+export * from './WordFormDTO';
